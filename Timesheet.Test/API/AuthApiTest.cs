4fd93fc5a9c4bb0e6e4c83a4044f10cab0a80766
dprushton <110337511+dprushton@users.noreply.github.com>
2024-11-12 12:48:07 +0000

    Added E2E test

--- Timesheet.Test/API/AuthApiTest.cs
+++ Timesheet.Test/API/AuthApiTest.cs
@@ -1,11 +1,25 @@
 ﻿namespace Timesheet.Test.Api;
 
+using static RestAssured.Dsl;
+using System.Net;
+
 public class AuthAPITest
 {
 
     [Test]
     public void TestLoginReturnsPositiveResponse()
     {
+        Login login = new("admin@test.com", "password123");
+
+        HttpResponseMessage response = Given()
+                                        .Body(login)
+                                        .ContentType("application/json")
+                                        .Post("http://localhost:8080/v1/auth/login")
+                                        .Then()
+                                        .Extract()
+                                        .Response();
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
 
     }
 
